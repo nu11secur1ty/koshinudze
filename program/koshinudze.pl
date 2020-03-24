9dada5c3d3d4b99b645bf8b4228184df5efb3dfb
nu11secur1ty <venvaropt@gmail.com>
2020-03-24 04:51:41 -0400

    koshinudze-v-1.1

--- program/koshinudze.pl
+++ program/koshinudze.pl
@@ -1,8 +1,36 @@
 #!/usr/bin/perl
 # by @nu11secur1ty > Penetration testing engineer
-# v-1.0
+# v-1.1
 use strict;
 use warnings;
 use diagnostics;
+use Term::ANSIColor;
+
+print color('red');
+print "Choice module\n";
+print color('reset');
+
+print color('green');
+	print "[1]scan\n";
+	print "[2]attack\n";
+	print "[3]sniff\n";
+print color('reset');
+
+my $m0dulEs = <STDIN>;
+
+if ($m0dulEs == 1){
 system("perl modules/scan.pl");
-exit 0;
+}else{
+	exit 0;
+}
+if ($m0dulEs == 2){
+system("perl modules/scan.pl");
+}else{
+        exit 0;
+}
+if ($m0dulEs == 3){
+system("perl modules/scan.pl");
+}else{
+        exit 0;
+}
+	exit 0;
