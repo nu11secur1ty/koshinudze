38cfb5222bc8edfdc3dbd1d8f88282088e848131
nu11secur1ty <venvaropt@gmail.com>
2020-03-27 05:56:08 -0400

    koshinudze-v-1.1

--- program/modules/attack.pl
+++ program/modules/attack.pl
@@ -3,7 +3,11 @@ use warnings;
 use strict;
 use diagnostics;
 
+my @m0dules = ([my $m1 = `ping 8.8.8.8 -c 3`, 
+		my $m2, 
+		my $m3, 
+		my $m4, 
+		my $m5]
+);
+print "$m0dules[0][0]\n";
 
-my $m0de1 = `ping 8.8.8.8 -c 3`;
-print "$m0de1\n";
-exit 0;
