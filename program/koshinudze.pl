29dffe1efd22f6b3da16322fd492404f7a5909aa
nu11secur1ty <venvaropt@gmail.com>
2020-03-27 05:34:52 -0400

    koshinudze-v-1.1

--- program/koshinudze.pl
+++ program/koshinudze.pl
@@ -14,23 +14,18 @@ print color('green');
 	print "[1]scan\n";
 	print "[2]attack\n";
 	print "[3]sniff\n";
+	print "For EXIR press Ctrl + C\n";
 print color('reset');
 
 my $m0dulEs = <STDIN>;
 
 if ($m0dulEs == 1){
 system("perl modules/scan.pl");
-}else{
-	exit 0;
 }
 if ($m0dulEs == 2){
 system("perl modules/attack.pl");
-}else{
-        exit 0;
 }
 if ($m0dulEs == 3){
 system("perl modules/sniff.pl");
-}else{
-        exit 0;
 }
 	exit 0;
---
+++ program/modules/attack.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use diagnostics;
+
+
+my $m0de1 = `ping 8.8.8.8 -c 3`;
+print "$m0de1\n";
+exit 0;
