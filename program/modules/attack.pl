6552648033c42626252675694f1b6401fb6e7c18
nu11secur1ty <venvaropt@gmail.com>
2020-03-28 12:44:01 -0400

    koshinudze-v-1.1

--- program/modules/attack.pl
+++ program/modules/attack.pl
@@ -3,11 +3,13 @@ use warnings;
 use strict;
 use diagnostics;
 
-my @attack_m0de = ([my $m1 = `ping 127.0.0.1 -c 3`, 
-		my $m2, 
-		my $m3, 
-		my $m4, 
-		my $m5]
+my @attack_m0de = (
+		[my $m1 = `ping 127.0.0.1 -c 3`], 
+		[my $m2 = ``], 
+		[my $m3 = ``], 
+		[my $m4 = ``], 
+		[my $m5 = ``]
 );
 print "$attack_m0de[0][0]\n";
+print "$attack_m0de[4][0]\n";
 
