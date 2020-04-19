ecc49d396f82cde24bbd3a0cbf1c35bc18cff004
nu11secur1ty <venvaropt@gmail.com>
2020-04-19 10:53:19 +0300

    Update cmsother.pl

--- program/modules/cmsother.pl
+++ program/modules/cmsother.pl
@@ -5,14 +5,19 @@ use strict;
 use diagnostics;
 
 print "Give URL target for example: http://localhost:8080/\n";
+
 chomp (my $URL_target = <STDIN>);
 my @attack_m0de = (
 		[my $m1 = `wad -u $target_`], 
 );
+
+print color(green);
 print "$attack_m0de[0][0]\n";
+print "---------------------------------\n";
+print color(reset);
 
 # Save
-my $filename = 'koshinudze_traversal.log';
+my $filename = 'koshinudze_cmsother.log';
 open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
         print $fh "$m1\n";
         close $fh;
