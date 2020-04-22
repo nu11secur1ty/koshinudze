545159d1a1749fe72fe229c0dd5268ff2dd7e37e
nu11secur1ty <venvaropt@gmail.com>
2020-04-22 23:44:01 +0300

    Update scan.pl

--- program/modules/scan.pl
+++ program/modules/scan.pl
@@ -7,7 +7,7 @@ use Term::ANSIColor;
 # use 5.010;
 
 print "Give the target host or IP, for example: example.com or 0.0.0.0\n";
-print color('orange');
+print color('black');
 print "NOTE: Please wait after your choice, koshinudze is working for you...\n";
 print color('reset');
 
