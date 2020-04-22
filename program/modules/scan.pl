a17757c0260e7d243354263514d48afea07d8e47
nu11secur1ty <venvaropt@gmail.com>
2020-04-22 23:12:40 +0300

    Update scan.pl

--- program/modules/scan.pl
+++ program/modules/scan.pl
@@ -15,7 +15,7 @@ our @koshinudze_ = (
 	[ my $m0de5 = `nmap -sU -A -PN -n -pU:19,53,123,161 -script=ntp-monlist,dns-recursion,snmp-sysdescr $target_` ],
 	[ my $m0de6 = `nmap -sV --script=smb* $target_`, my $m0de7 = `nmap -sC -sV -oA $target_` ],
 	[ my $m0de8 = `dig $target_`, my $m0de9 = `traceroute $target_`, my $m0de10 = `whois $target_`, my $m0de11 = `geoiplookup $target_` ],
-	[ my $m0de12 = `whatweb $target_` ]
+	[ my $m0de12 = `whatweb $target_`, my $m0de13 =`nikto -h $target_ -output n1kto_output.log` ]
 );
 
 print "Your result\n";
@@ -70,6 +70,11 @@ print "Who\n";
 	
 print "Host info structure\n";
 	print "$koshinudze_[5][0]\n";
+	print "----------------------------------------------------------\n";
+print "Advanced with Nikto\n";
+	print color('silver');
+	print "$koshinudze_[5][1]\n";
+	print color('reset');
 
 # Save
 my $filename = 'koshinudze_scan.log';
